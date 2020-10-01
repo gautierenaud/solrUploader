cbb5cf5da1eae5c4f193b43bc3c122ff8c5dec92
renaud <gautierenaud@gmail.com>
2020-10-01 22:38:30 +0200

    Enhance a bit the search popup

--- extensions/mozilla/popup/choose_page.js
+++ extensions/mozilla/popup/choose_page.js
@@ -19,23 +19,43 @@ document.addEventListener("click", function(e) {
         console.log(xhr.getAllResponseHeaders())
     }
 
-    xhr.open('GET', `http://localhost:8899/solr/documents/query?debug=query&q=text:*${searchText}*&wt=json`, true);
+    xhr.open('GET', `http://localhost:8899/solr/documents/query?debug=query&q=text:${searchText}~2&hl.fl=text&hl=on&usePhraseHighLighter=true&wt=json`, true);
     xhr.send();
 });
 
+// Get the input field
+var input = document.getElementById("searchbox");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("nana").click();
+    }
+});
+
 function displayResults(results) {
-    let resultDiv = document.getElementById("result");
+    let resultDivs = document.getElementById("result");
 
     // remove previous results
-    while (resultDiv.firstChild) {
-        resultDiv.removeChild(resultDiv.lastChild);
+    while (resultDivs.firstChild) {
+        resultDivs.removeChild(resultDivs.lastChild);
     }
 
     for (const doc of results["response"]["docs"]) {
+        var resultDiv = document.createElement("div");
+
         var newDiv = document.createElement("div");
         var newContent = document.createTextNode(doc["filename"]);
         newDiv.appendChild(newContent);
+        resultDiv.appendChild(newDiv);
 
-        resultDiv.appendChild(newDiv)
+        var newHighlight = document.createElement("pre");
+        newHighlight.innerHTML = results["highlighting"][doc["id"]]["text"];
+        resultDiv.appendChild(newHighlight);
+
+        resultDivs.appendChild(resultDiv);
     }
 }
